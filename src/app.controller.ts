3ebba5917aa08c3f77311f80e25023d7f0542e8b
HamzaBhatti125 <hbhati006@gmail.com>
2021-11-29 13:21:48 +0500

    jwt strategy implemented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,22 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AuthenticatedGuard } from './auth/authenticated.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req): any {
-    return { msg: 'Logged In!' };
+    return this.authService.login(req.user);
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtAuthGuard)
   @Get('protected')
   getHello(@Request() req): string {
+    //TODO: require access token
     return req.user;
   }
 }
